a57cb7d48498a6968d9737faee9ddbac43579ea5
pmisiak1 <pmisiak1@sii.pl>
2023-11-30 11:08:25 +0100

    navigation

--- ToRemove/DetailPage.xaml.cs
+++ ToRemove/DetailPage.xaml.cs
@@ -5,10 +5,19 @@ public partial class DetailPage : ContentPage
 	public DetailPage()
 	{
 		InitializeComponent();
-		
 	}
 
 	private void CounterBtn_OnClicked(object? sender, EventArgs e)
+	{
+		Navigation.PushAsync(new TestPage());
+	}
+
+	private void TapGestureRecognizer_OnTapped(object? sender, TappedEventArgs e)
+	{
+		Navigation.PushAsync(new TestPage());
+	}
+
+	private void SwipeGestureRecognizer_OnSwiped(object? sender, SwipedEventArgs e)
 	{
 		Navigation.PushAsync(new TestPage());
 	}
